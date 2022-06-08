00078ead224bd664df41c880dbda2e5582ae7a80
madmax7896 <madmax78968@gmail.com>
2022-06-08 20:56:33 +0530

    sm6375-common: perf: Import holi msm_irqbalance.conf
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- holi.mk
+++ holi.mk
@@ -273,6 +273,10 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
