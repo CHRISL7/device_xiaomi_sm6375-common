a26ef5e0a3a958c24b8fb8826622643dc0db9e59
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-07-28 20:38:09 +0000

    sm6375-common: Add Treble flags definition
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- holi.mk
+++ holi.mk
@@ -460,6 +460,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# Treble
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
