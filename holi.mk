ec46cae8bc4506aa59c1d35559b2e0f2bc19455a
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-08-04 16:45:50 +0000

    sm6375-common: props: Rework properties
    [1] - Move to makefie
    [2] - Remove duplicate
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- holi.mk
+++ holi.mk
@@ -42,11 +42,77 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.config.vc_call_vol_steps=11
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.audio.monitorRotation=true \
+    ro.vendor.audio.sos=true \
+    ro.vendor.audio.game.mode=true \
+    ro.vendor.audio.voice.volume.boost=manual \
+    ro.vendor.audio.afe.record=true \
+    ro.vendor.audio.us.proximity=true \
+    ro.vendor.audio.voice.change.support=true \
+    ro.vendor.audio.soundfx.type=mi \
+    ro.vendor.audio.vocal.support=true \
+    ro.vendor.audio.surround.support=true \
+    ro.vendor.audio.scenario.support=true \
+    ro.vendor.audio.sfx.scenario=true \
+    ro.vendor.audio.spk.stereo=true \
+    ro.vendor.audio.sfx.earadj=true \
+    ro.vendor.audio.soundfx.usb=true \
+    ro.vendor.audio.miui.karaoke.show=false \
+    ro.vendor.audio.miui.karaoke.tencent.show=false \
+    ro.vendor.audio.voice.change.youme.support=true \
+    persist.vendor.audio.misound.disable=true \
+    persist.vendor.audio.delta.refresh=true \
+    vendor.audio.chk.cal.spk=2 \
+    vendor.audio.chk.cal.us=2
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.audio.soundtrigger.xiaomievent=1 \
+    ro.vendor.audio.soundtrigger.pangaea=1 \
+    ro.vendor.audio.soundtrigger.sva-5.0=1 \
+    ro.vendor.audio.soundtrigger=sva \
+    ro.vendor.audio.soundtrigger.lowpower=true \
+    ro.vendor.audio.soundtrigger.training.level=50 \
+    ro.vendor.audio.soundtrigger.xanzn.gmm.level=40 \
+    ro.vendor.audio.soundtrigger.xanzn.gmm.user.level=30 \
+    ro.vendor.audio.soundtrigger.xanzn.cnn.level=70 \
+    ro.vendor.audio.soundtrigger.xanzn.vop.level=10 \
+    ro.vendor.audio.soundtrigger.xatx.gmm.level=30 \
+    ro.vendor.audio.soundtrigger.xatx.gmm.user.level=40 \
+    ro.vendor.audio.soundtrigger.xatx.cnn.level=45 \
+    ro.vendor.audio.soundtrigger.xatx.vop.level=0 \
+    ro.vendor.audio.soundtrigger.appdefine.gmm.level=55 \
+    ro.vendor.audio.soundtrigger.appdefine.gmm.user.level=65 \
+    ro.vendor.audio.soundtrigger.appdefine.cnn.level=45 \
+    ro.vendor.audio.soundtrigger.appdefine.vop.level=10 \
+    ro.vendor.audio.soundtrigger.snr=16
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti \
     android.hardware.boot@1.2-impl-qti.recovery \
     android.hardware.boot@1.2-service
+
+# Bluetooth
+PRODUCT_PRODUCT_PROPERTIES  += \
+    bluetooth.hardware.power.operating_voltage_mv=3700 \
+    bluetooth.hardware.power.idle_cur_ma=3 \
+    bluetooth.hardware.power.rx_cur_ma=20 \
+    bluetooth.hardware.power.tx_cur_ma=36
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.bluetooth.defaultabsvol=false \
+    ro.vendor.bluetooth.wipower=false \
+    persist.bluetooth.disableabsvol=true \
+    persist.vendor.bluetooth.modem_nv_support=true \
+    persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.scram.enabled=false \
+    persist.vendor.qcom.bluetooth.twsp_state.enabled=false
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -57,10 +123,22 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+PRODUCT_VENDOR_PROPERTIES += \
+    camera.disable_zsl_mode=1
+
+# Charger
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.charge.oneTrack=true
+
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
+
+# Crypto
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.metadata.method=dm-default-key
 
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
@@ -70,10 +148,28 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     libdisplayconfig.qti
 
+PRODUCT_VENDOR_PROPERTIES += \
+    debug.hwui.use_hint_manager=true \
+    debug.hwui.target_cpu_time_percent=30 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.enable_transaction_tracing=false \
+    ro.vendor.sf.enable_fb_scaling=1 \
+    vendor.display.enable_perf_hint_large_comp_cycle=1 \
+    vendor.display.enable_posted_start_dyn=2 \
+    vendor.display.idle_time=1100
+
+# DPM
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.dpm.nsrm.bkg.evt=3955 \
+    persist.vendor.dpmhalservice.enable=1
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
+
+PRODUCT_VENDOR_PROPERTIES += \
+    drm.service.enabled=true
 
 # Fastbootd
 PRODUCT_PACKAGES += \
@@ -85,17 +181,31 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    sys.fp.miui.token=0
+
 # FM
 BOARD_HAVE_QCOM_FM := true
+
+# FRP
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.gatekeeper.disable_spu=true
+
 # Input
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/idc/,$(TARGET_COPY_OUT_SYSTEM)/usr/idc) \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout)
+
+# Incremental FS
+PRODUCT_VENDOR_OVERRIDES += \
+    ro.incremental.enable=1
 
 # Keymaster
 PRODUCT_PACKAGES += \
@@ -108,9 +218,34 @@ PRODUCT_PACKAGES += \
     libcodec2_soft_common.vendor \
     libsfplugin_ccodec_utils.vendor
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.media.transcoding.codec_max_operating_rate_720P=240 \
+    debug.media.transcoding.codec_max_operating_rate_1080P=120
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    media.aac_51_output_enabled=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-scan=true \
+    mmp.enable.3g2=true \
+    persist.mm.enable.prefetch=true
+
+PRODUCT_VENDOR_PROPERTIES += \
+    debug.stagefright.c2inputsurface=-1
+
 # Neural networks
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
+
+# NFC
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.nfc.port=I2C
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.se.type=HCE,UICC
 
 # Overlays
 PRODUCT_PACKAGES += \
@@ -177,6 +312,24 @@ TARGET_COMMON_QTI_COMPONENTS += \
     vibrator \
     wfd
 
+# Radio
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.data.iwlan.enable=true \
+    persist.vendor.radio.atfwd.start=true \
+    persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.dynamic_load_mbn=3 \
+    persist.vendor.radio.dynamic_sar=1 \
+    persist.vendor.radio.enable_temp_dds=true \
+    persist.vendor.radio.force_ltd_sys_ind=1 \
+    persist.vendor.radio.force_on_dc=true \
+    persist.vendor.radio.hidl_dev_service=true \
+    persist.vendor.radio.manual_nw_rej_ct=1 \
+    persist.vendor.radio.mbn_trace=true \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.default_cdma_sub=0 \
+    ro.vendor.radio.5g=3 \
+    ro.vendor.use_data_netmgrd=true
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.batterysecret.rc \
@@ -218,6 +371,14 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
 
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.sensors.debug.ssc_qmi_debug=true \
+    persist.vendor.sensors.hal_trigger_ssr=false \
+    persist.vendor.sensors.enable.rt_task=false \
+    persist.vendor.sensors.support_direct_channel=false \
+    persist.vendor.sensors.enable.bypass_worker=true \
+    persist.vendor.sensors.sync_request=true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
@@ -226,6 +387,9 @@ PRODUCT_SOONG_NAMESPACES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.sys.thermal.data.path=/data/vendor/thermal/
 
 # Update engine
 PRODUCT_PACKAGES += \
@@ -265,9 +429,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.wlan.dbs=0 \
+    wifi.aware.interface=wifi-aware0
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libwfdaac_vendor
+
+# Zygote
+PRODUCT_SYSTEM_PROPERTIES += \
+    zygote.critical_window.minute=10
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm6375-common/sm6375-common-vendor.mk)
